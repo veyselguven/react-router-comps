81d6ca65656639eb0512cbd55e46f425d75f9339
veyselguven <vgbasbaydar@gmail.com>
2023-04-02 13:09:04 -0700

    add buttons comp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Button from "./Button";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <Button success outline rounded>
+          Click Me
+        </Button>
+      </div>
+      <div>
+        <Button danger outline>
+          Buy Now
+        </Button>
+      </div>
+      <div>
+        <Button warning>See Deail</Button>
+      </div>
+      <div>
+        <Button secondary outline>
+          Hi dead
+        </Button>
+      </div>
+      <div>
+        <Button secondary rounded>
+          Whats up
+        </Button>
+      </div>
     </div>
   );
 }
---
+++ src/Button.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Button({
+  children,
+  primary,
+  secondary,
+  success,
+  warning,
+  danger,
+  outline,
+  rounded,
+}) {
+  return <button>{children}</button>;
+}
+
+export default Button;
