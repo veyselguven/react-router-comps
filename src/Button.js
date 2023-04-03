c8216076b7bfcb3a982825b57d730e4a8c4e50e2
veyselguven <vgbasbaydar@gmail.com>
2023-04-03 16:26:01 -0700

    add some npm package

--- src/Button.js
+++ src/Button.js
@@ -10,7 +10,24 @@ function Button({
   outline,
   rounded,
 }) {
-  return <button>{children}</button>;
+  return <button className="">{children}</button>;
 }
 
+Button.propTypes = {
+  checkVariationValue: ({ primary, secondary, success, warning, danger }) => {
+    // console.log(props);
+    const count =
+      Number(!!primary) +
+      Number(!!secondary) +
+      Number(!!warning) +
+      Number(!!success) +
+      Number(!!danger);
+
+    if (count > 1) {
+      return new Error(
+        "Only one of primay,secondary,succes,warning,danger can be true "
+      );
+    }
+  },
+};
 export default Button;
