d3552df2cb47ffe339403df8aa2a01a8fc5b68a8
veyselguven <vgbasbaydar@gmail.com>
2023-04-10 23:02:47 -0700

    add some style to accordion comp

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,29 +1,37 @@
 import React from "react";
 import { useState } from "react";
+import { GoChevronLeft, GoChevronDown } from "react-icons/go";
 
 function Accordion({ items }) {
   const [expandedIndex, setExpandedIndex] = useState(0);
+  const handleClick = (nextIndex) => {
+    setExpandedIndex(nextIndex);
+  };
 
-  console.log(items);
+  //console.log(items);
   const renderedItem = items.map((item, index) => {
-    // if (index === expandedIndex) {
-    //   console.log("expanded");
-    // } else {
-    //   console.log("collapsed");
-    // }
-
     const isExpanded = index === expandedIndex;
-
-    console.log(isExpanded);
+    const icon = (
+      <span className="text-2xl">
+        {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
+      </span>
+    );
+    // console.log(isExpanded);
     return (
       <div key={index}>
-        <div>{item.label}</div>
-        {isExpanded && <div>{item.content}</div>}
+        <div
+          className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer "
+          onClick={() => handleClick(index)}
+        >
+          {item.label}
+          {icon}
+        </div>
+        {isExpanded && <div className="border-b p-5">{item.content}</div>}
       </div>
     );
   });
 
-  return <div>{renderedItem}</div>;
+  return <div className="border-x border-t rounded">{renderedItem}</div>;
 }
 
 export default Accordion;
