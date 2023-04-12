635981d55664ba241b7ce5ac43380b8b02656dd5
veyselguven <vgbasbaydar@gmail.com>
2023-04-12 13:49:02 -0700

    added feature to dropdown component

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Dropdown({ options }) {
-  return <div>Dropdownnn.dwafsf...</div>;
+  const [isOpen, setIsOpen] = useState(false);
+  const handleOptionClick = (option) => {
+    // close dropdown
+    setIsOpen(false);
+    // what option did user click on ??
+    console.log(option);
+  };
+
+  const handleClick = (event) => {
+    setIsOpen(!isOpen);
+  };
+
+  const renderedOptions = options.map((option) => {
+    return (
+      <div onClick={() => handleOptionClick(option)} key={option.value}>
+        {option.label}
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div onClick={handleClick}>Select..</div>
+      {isOpen && <div>{renderedOptions}</div>}
+    </div>
+  );
 }
 
 export default Dropdown;
