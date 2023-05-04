70479a21d7fb27dea845672200b23a161166f735
veyselguven <vgbasbaydar@gmail.com>
2023-05-04 12:23:26 -0700

    custonusenavigation hooks added

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,12 +1,27 @@
 import React from "react";
-import { useContext } from "react";
-import NavigationContext from "../context/navigation";
+// import { useContext } from "react";
+// import NavigationContext from "../context/navigation";
+import useNavigation from "../hooks/use-navigation";
+import classNames from "classnames";
 
 export default function Link({ to, children }) {
-  const { navigate } = useContext(NavigationContext);
+  // const { navigate } = useContext(NavigationContext);
+  const { navigate } = useNavigation();
+
+  const classes = classNames("text-blue-500");
+
   const handleClick = (event) => {
+    // console.log(event);
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
     event.preventDefault();
+
     navigate(to);
   };
-  return <a onClick={handleClick}> {children}</a>;
+  return (
+    <a className={classes} href={to} onClick={handleClick}>
+      {children}
+    </a>
+  );
 }
