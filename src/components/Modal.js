8e7c9487f1b38ccaeebbee95dfacf028efaddc7a
veyselguven <vgbasbaydar@gmail.com>
2023-05-06 18:03:10 -0700

    added some css

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Modal() {
-  return <div>Modal</div>;
+  return (
+    <div>
+      <div className="absolute inset-0 bg-gray-300 opacity-80"></div>
+      <div className="absolute inset-40 p-10 bg-white">I am a Modall</div>
+    </div>
+  );
 }
 
 export default Modal;
