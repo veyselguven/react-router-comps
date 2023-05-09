14f3d73b1f5a7ccb0bc906e9b7918fa59af5412f
veyselguven <vgbasbaydar@gmail.com>
2023-05-09 16:17:19 -0700

    added style to table comp

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,12 +1,28 @@
 import React from "react";
 
 function Table({ data }) {
-  //console.log(data);
+  const renderedRows = data.map((fruit) => {
+    return (
+      <tr className="border-b" key={fruit.name}>
+        <td className="p-3">{fruit.name}</td>
+        <td className="p-3">
+          <div className={`p-3 m-2 ${fruit.color}`}></div>
+        </td>
+        <td className="p-3">{fruit.score}</td>
+      </tr>
+    );
+  });
+
   return (
-    <table>
-      <div>sdfgs</div>
-      <thead></thead>
-      <tbody></tbody>
+    <table className="table-auto border-spacing-2">
+      <thead>
+        <tr className="border-b-2">
+          <th>Fruit</th>
+          <th>Color</th>
+          <th>Score</th>
+        </tr>
+      </thead>
+      <tbody>{renderedRows}</tbody>
     </table>
   );
 }
