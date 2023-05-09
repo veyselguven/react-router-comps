9f0220f3e39d7d6a44eeca6a4d48f76a80a91716
veyselguven <vgbasbaydar@gmail.com>
2023-05-09 09:03:24 -0700

    added model comp feature

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,21 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
-function Modal() {
-  return (
+function Modal({ onClose, children, actionBar }) {
+  return ReactDOM.createPortal(
     <div>
-      <div className="absolute inset-0 bg-gray-300 opacity-80"></div>
-      <div className="absolute inset-40 p-10 bg-white">I am a Modall</div>
-    </div>
+      <div
+        className="absolute inset-0 bg-gray-300 opacity-80"
+        onClick={onClose}
+      ></div>
+      <div className="absolute inset-40 p-10 bg-white">
+        <div className="flex flex-col justify-between h-full">
+          {children}
+          <div className="flex justify-end">{actionBar}</div>
+        </div>
+      </div>
+    </div>,
+    document.querySelector(".modal-container")
   );
 }
 
--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -9,12 +9,42 @@ function ModalPage() {
   const handleClick = () => {
     setShowModal(true);
   };
-  return (
+
+  const handleClose = () => {
+    setShowModal(false);
+  };
+  const actionBar = (
     <div>
+      <Button onClick={handleClose} primary>
+        I accept
+      </Button>
+    </div>
+  );
+  const modal = (
+    <Modal onClose={handleClose} actionBar={actionBar}>
+      <p>Here is an important agreement for you to accept</p>
+    </Modal>
+  );
+
+  return (
+    <div className="relative">
       <Button onClick={handleClick} primary>
         Open Modal
       </Button>
-      {showModal && <Modal />}
+      {showModal && modal}
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+        imperdiet arcu sem, ac imperdiet urna venenatis at. Praesent ac lacinia
+        nulla, quis imperdiet orci. Nam sit amet mauris a nibh auctor faucibus
+        in et ex. Sed sagittis at dolor nec molestie. Cras enim nisl, sodales
+        vel semper eu, auctor nec massa. Nam id ligula a dolor posuere consequat
+        non quis elit. In hac habitasse platea dictumst. Aenean malesuada ac mi
+        non ullamcorper. Aliquam accumsan lectus tristique lorem fermentum, in
+        ultrices dui rhoncus. Nulla nec pretium odio. Morbi semper molestie
+        massa, vitae finibus sem venenatis sit amet. Interdum et malesuada fames
+        ac ante ipsum primis in faucibus. Nulla tincidunt leo a mi vehicula,
+        tempor pellentesque neque facilisis.
+      </p>
     </div>
   );
 }
