9fdaa5b98493726066d15fcab6ebc26a9a6912a1
veyselguven <vgbasbaydar@gmail.com>
2023-05-15 12:25:45 -0700

    config file

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -10,9 +10,9 @@ function TablePage() {
   ];
 
   const config = [
-    { label: "Name of Fruit" },
-    { label: "Color" },
-    { label: "Score" },
+    { label: "Name", render: (fruit) => fruit.name },
+    { label: "Color", render: (fruit) => fruit.color },
+    { label: "Score", render: (fruit) => fruit.score },
   ];
 
   return (
