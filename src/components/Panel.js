d25399b8bd6b5b3b0439d5d13ec0047f9e33b199
veyselguven <vgbasbaydar@gmail.com>
2023-07-07 21:29:18 -0700

    changed from useSte to the useReducer

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -3,7 +3,7 @@ import React from "react";
 import classNames from "classnames";
 
 function Panel({ children, className, ...rest }) {
-  console.log(rest);
+  // console.log(rest);
 
   const finalClassNames = classNames(
     "border rounded p-3 shadow bg-white w-full",
--- src/pages/CounterPages.js
+++ src/pages/CounterPages.js
@@ -1,33 +1,39 @@
 import Button from "../components/Button";
 import Panel from "../components/Panel";
 
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
+
+const reducer = (state, action) => {};
 
 function CounterPages({ initialCount }) {
-  const [count, setCount] = useState(initialCount);
-  const [valueToAdd, setValueToAdd] = useState(0);
-
+  // const [count, setCount] = useState(initialCount);
+  // const [valueToAdd, setValueToAdd] = useState(0);
+  const [state, dispatch] = useReducer(reducer, {
+    count: initialCount,
+    valueToAdd: 0,
+  });
+  console.log(state);
   const increment = () => {
-    setCount(count + 1);
+    // setCount(count + 1);
   };
   const decrement = () => {
-    setCount(count - 1);
+    // setCount(count - 1);
   };
   const handleChange = (e) => {
     const value = parseInt(e.target.value) || 0;
     // console.log(typeof value);
     // console.log(value);
-    setValueToAdd(value);
+    // setValueToAdd(value);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setCount(count + valueToAdd);
-    setValueToAdd(0);
+    // setCount(count + valueToAdd);
+    // setValueToAdd(0);
   };
   return (
     <Panel className="m-3">
-      <h1 className="text-lg">Count is {count}</h1>
+      <h1 className="text-lg">Count is {state.count}</h1>
       <div className="flex flex-row">
         <Button onClick={increment}>Increment</Button>
         <Button onClick={decrement}>Decrement</Button>
@@ -35,7 +41,7 @@ function CounterPages({ initialCount }) {
       <form onSubmit={handleSubmit}>
         <label>Add a lot!</label>
         <input
-          value={valueToAdd || ""}
+          value={state.valueToAdd || ""}
           onChange={handleChange}
           type="number"
           className="p-1 m-3 bg-gray-50 border border-gray-300"
